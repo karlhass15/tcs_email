207ad145019b2273be99a39ddde437a37366b9f9
karlhass15 <karl@reclaimedartcraftsman.com>
2015-12-14 08:36:26 -0600

    heroku testing

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var path = require('path');
 
 
-router.get("/*", function(req, res, next){
+router.get("/", function(req, res, next){
     var file = req.params[0] || 'assets/views/index.html';
     res.sendFile(path.join(__dirname, "../public", file));
 });
